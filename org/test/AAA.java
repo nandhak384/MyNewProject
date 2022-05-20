f04e4dd57fbb261958c9b953fa8181c4b423f174
nandhak384 <kumar230597@gmail.com>
2022-05-20 13:56:37 +0530

    MyFirstcommit

---
+++ org/test/AAA.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class AAA {
+	int a = 25, b = 45;
+	int c = a + b;
+
+	public static void main(String[] args) {
+		AAA x = new AAA();
+		System.out.println(x.c);
+	}
+
+}
